adc9642fa0c59df38a318b81d1d76a9d898bfdeb
Tomas Hurka <tomas.hurka@oracle.com>
2024-11-22 17:44:19 +0100

    GDK 4.6.0.4 FCS

--- gdk/build.gradle.kts
+++ gdk/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 }
 
 group = "cloud.graal.gdk.plugin"
-version = "1.3.0"
+version = "1.4.0"
 
 repositories {
   mavenLocal()
@@ -39,7 +39,7 @@ intellij {
 }
 
 dependencies {
-  implementation("cloud.graal.gdk:gdk-core:4.6.0.3") {
+  implementation("cloud.graal.gdk:gdk-core:4.6.0.4") {
         exclude(group="org.slf4j", module="slf4j-api")
   }
 
