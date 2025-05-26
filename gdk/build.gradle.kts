5668e59059b14e96b12d6ee7a2a7c4b12f4a19c6
Tomas Hurka <tomas.hurka@oracle.com>
2025-05-26 14:42:09 +0200

    buildSearchableOptions disabled

--- gdk/build.gradle.kts
+++ gdk/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
+    buildSearchableOptions = false
     pluginConfiguration {
         version = providers.gradleProperty("pluginVersion")
 
